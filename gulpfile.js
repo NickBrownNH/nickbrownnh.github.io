f5f61f9521180f7ced7e230f193575fcf381d977
Nicholas Brown <nb@brownsfromsa.com>
2025-05-27 15:21:18 -0400

    Added htmlCompressor, basic info in index.html, and added a gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
-const {src, dest, watch, series} = require(`gulp`);
+const {src, dest, watch, series} = require(`gulp`),
+    htmlCompressor = require(`gulp-htmlmin`);
+
+let compressHTML = () => {
+    return src(`app/html/*.html`)
+        .pipe(htmlCompressor({ collapseWhitespace: true }))
+        .pipe(dest(`prod`));
+};
+
+exports.compressHTML = compressHTML;
